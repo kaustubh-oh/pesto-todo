e908437b859fb6baa43cca1027ea7f2f4881865f
Kaustubh Agrawal <kaustubh@openhouse.study>
2023-11-17 08:47:27 +0530

    created a todo app

---
+++ apps/todo/vite.config.ts
@@ -0,0 +1,29 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  cacheDir: '../../node_modules/.vite/todo',
+
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+
+  plugins: [react(), nxViteTsPaths()],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  define: {
+    'import.meta.vitest': undefined,
+  },
+});
