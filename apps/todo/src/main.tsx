72937ae85f3345531927ddb91d050649e083a134
Kaustubh Agrawal <kaustubh@openhouse.study>
2023-11-17 09:09:58 +0530

    Mui Setup

--- apps/todo/src/main.tsx
+++ apps/todo/src/main.tsx
@@ -1,13 +1,22 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@mui/material';
 
 import App from './app/App';
+
+import '@fontsource/nunito/300.css';
+import '@fontsource/nunito/400.css';
+import '@fontsource/nunito/500.css';
+import '@fontsource/nunito/700.css';
+import { theme } from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
