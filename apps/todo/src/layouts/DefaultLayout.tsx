7887e91fa7b08869e50e6d74ac6381071be3ef1e
Kaustubh Agrawal <kaustubh@openhouse.study>
2023-11-18 15:23:40 +0530

    basic layout change

---
+++ apps/todo/src/layouts/DefaultLayout.tsx
@@ -0,0 +1,156 @@
+import { Typography } from '@mui/material';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Toolbar from '@mui/material/Toolbar';
+import { styled, useTheme } from '@mui/material/styles';
+import * as React from 'react';
+import {
+  MdChevronLeft,
+  MdChevronRight,
+  MdInbox,
+  MdMail,
+  MdMenu,
+} from 'react-icons/md';
+
+const drawerWidth = 320;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create('margin', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: `-${drawerWidth}px`,
+  ...(open && {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+interface DefaultLayoutProps {
+  children: React.ReactNode;
+}
+
+export function DefaultLayout({ children }: DefaultLayoutProps) {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        color={'transparent'}
+        sx={{ boxShadow: 'none' }}
+        open={open}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MdMenu />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <Typography
+            variant="h5"
+            px={1}
+            fontWeight={'bold'}
+            sx={theme.direction === 'ltr' ? { mr: 'auto' } : { ml: 'auto' }}
+          >
+            Lists
+          </Typography>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <MdChevronLeft /> : <MdChevronRight />}
+          </IconButton>
+        </DrawerHeader>
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <MdInbox /> : <MdMail />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+        {children}
+      </Main>
+    </Box>
+  );
+}
--- apps/todo/src/pages/Home.tsx
+++ apps/todo/src/pages/Home.tsx
@@ -1,17 +1,11 @@
-import {
-  Box,
-  Container,
-  Divider,
-  RadioGroup,
-  Stack,
-  Typography,
-} from '@mui/material';
+import { Container, RadioGroup, Stack, Typography } from '@mui/material';
+import { DefaultLayout } from '../layouts/DefaultLayout';
 
 export function Home() {
-  return (
+  const mainBody = (
     <Container maxWidth="md">
       <Stack>
-        <Stack py={4} justifyContent={'center'} alignItems={'center'}>
+        <Stack pb={4} justifyContent={'center'} alignItems={'center'}>
           <Typography variant="h3">To Do List</Typography>
           <Typography
             variant="caption"
@@ -27,4 +21,6 @@ export function Home() {
       </Stack>
     </Container>
   );
+
+  return <DefaultLayout>{mainBody}</DefaultLayout>;
 }
