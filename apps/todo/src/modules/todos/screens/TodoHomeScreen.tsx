b9cbe845d4446be7742580b831729d2dcb9b5c8d
Kaustubh Agrawal <kaustubh@openhouse.study>
2023-11-19 10:13:51 +0530

    Filters

--- apps/todo/src/modules/todos/screens/TodoHomeScreen.tsx
+++ apps/todo/src/modules/todos/screens/TodoHomeScreen.tsx
@@ -1,18 +1,16 @@
-import { Box, Container, SpeedDial, Stack, Typography } from '@mui/material';
+import { Button, Container, SpeedDial, Stack, Typography } from '@mui/material';
+import { TASK_STATUS_ENUM } from '@pesto/shared';
 import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 import { PiPlus } from 'react-icons/pi';
 import { fetchAllTasks, fetchAllTasksQueryKeys } from '../../../logic';
-import { CreateTaskSchema, Task } from '../../../shared';
+import { CreateTaskSchema, TASK_STATUS_CONFIG, Task } from '../../../shared';
 import { BottomDrawer, TodoItems } from '../../../ui';
 import { TodoForm } from '../components/TodoForm';
-import { TASK_STATUS_ENUM } from '@pesto/shared';
 
 const allTasksQueryKeys = fetchAllTasksQueryKeys();
 
-enum TASK_FILTER_ENUM {
-  ALL = 'all',
-}
+const ALL_FILTERS = 'all';
 
 const defaultEditMode = {
   edit: false,
@@ -31,9 +29,13 @@ export function Home() {
 
   const [editMode, setEditMode] = useState(defaultEditMode);
 
-  const [filter, setFilter] = useState<TASK_STATUS_ENUM[]>(
-    Object.values(TASK_STATUS_ENUM)
-  );
+  const [statusFilters, setStatusFilters] = useState<
+    TASK_STATUS_ENUM | typeof ALL_FILTERS
+  >(ALL_FILTERS);
+
+  const filterByStatus = (status: TASK_STATUS_ENUM | typeof ALL_FILTERS) => {
+    setStatusFilters(status);
+  };
 
   const editTask = (task: Task) => {
     setEditMode({
@@ -61,6 +63,23 @@ export function Home() {
     resetEditor();
   };
 
+  const numCompleted = data
+    ? data.reduce(
+        (numCompleted, task) =>
+          (numCompleted +=
+            TASK_STATUS_CONFIG[task.status as TASK_STATUS_ENUM].type === 'DONE'
+              ? 1
+              : 0),
+        0
+      )
+    : 0;
+
+  const filteredData = data
+    ? data.filter(
+        (data) => statusFilters === ALL_FILTERS || data.status === statusFilters
+      )
+    : [];
+
   const mainBody = (
     <Container maxWidth="md">
       <Stack>
@@ -73,12 +92,43 @@ export function Home() {
             color={'gray'}
             textTransform={'uppercase'}
           >
-            8 items pending
+            {numCompleted}/{data?.length} tasks completed
           </Typography>
+          <Stack spacing={1} py={2} direction={'row'}>
+            <Button
+              size={'small'}
+              disableElevation
+              sx={{ minWidth: '4em', fontSize: '0.7em' }}
+              color="success"
+              variant={statusFilters === ALL_FILTERS ? 'contained' : 'outlined'}
+              onClick={() => filterByStatus(ALL_FILTERS)}
+            >
+              All
+            </Button>
+            {Object.values(TASK_STATUS_ENUM).map((status) => {
+              const config = TASK_STATUS_CONFIG[status];
+              return (
+                <Button
+                  size={'small'}
+                  onClick={() => filterByStatus(status)}
+                  disableElevation
+                  color={config.pallete}
+                  variant={statusFilters === status ? 'contained' : 'outlined'}
+                  startIcon={<config.icon color={config.color} />}
+                  sx={{
+                    minWidth: '4em',
+                    fontSize: '0.7em',
+                  }}
+                >
+                  {config.text}
+                </Button>
+              );
+            })}
+          </Stack>
         </Stack>
-        <Stack>
-          {!isLoading && data ? (
-            <TodoItems data={data} editTask={editTask} />
+        <Stack pb={10}>
+          {!isLoading && filteredData ? (
+            <TodoItems data={filteredData} editTask={editTask} />
           ) : (
             'Loading...'
           )}
--- apps/todo/src/shared/constants/index.ts
+++ apps/todo/src/shared/constants/index.ts
@@ -1,6 +1,8 @@
 import { green, grey, orange } from '@mui/material/colors';
 import { TASK_STATUS_ENUM } from '@pesto/shared';
 
+import { PiCheckCircleBold, PiCircleBold } from 'react-icons/pi';
+
 export const ENDPOINTS = {
   TODO: {
     LIST: '/todo-lists',
@@ -18,20 +20,26 @@ export enum REST_METHODS_ENUM {
 export const TASK_STATUS_CONFIG = {
   [TASK_STATUS_ENUM.TODO]: {
     text: 'To Do',
+    icon: PiCircleBold,
     type: 'TODO',
     color: grey[500],
+    pallete: 'inherit',
     next: TASK_STATUS_ENUM.IN_PROGRESS,
   },
   [TASK_STATUS_ENUM.IN_PROGRESS]: {
     text: 'In Progress',
+    icon: PiCircleBold,
     type: 'IN_PROGRESS',
     color: orange[500],
+    pallete: 'warning',
     next: TASK_STATUS_ENUM.DONE,
   },
   [TASK_STATUS_ENUM.DONE]: {
     text: 'Done',
+    icon: PiCheckCircleBold,
     type: 'DONE',
     color: green[500],
+    pallete: 'success',
     next: TASK_STATUS_ENUM.DONE,
   },
 };
